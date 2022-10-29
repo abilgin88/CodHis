9442bbb3ee42c07545afc26585dc910990a1c755
Abdullah Bilgin <abilgin02@icloud.com>
2022-10-29 10:57:04 +0300

    Creating a project in Xcode

---
+++ Code History/Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Abdullah Bilgin on 10/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
