5efed73723bb3bb18230235be41d06a619932a2d
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-02 04:59:07 +0300

    add more questions and store them in a single place separate from our view

--- Code History/Code History/Models/Question.swift
+++ Code History/Code History/Models/Question.swift
@@ -13,5 +13,23 @@ struct Question {
     let correctAnswerIndex: Int
     
     
+    static var allQuestions = [
+        Question(questionText: "Who invented the World Wide Web?",
+                 possibleAnswers: [
+                    "Steve Jobs",
+                    "Linus Torvalds",
+                    "Bill Gates",
+                    "Tim Berners-Lee"
+                 ],
+                 correctAnswerIndex: 3),
+        Question(questionText: "What was the first object oriented programming language?",
+                 possibleAnswers: [
+                    "Simula",
+                    "Python",
+                    "Swift",
+                    "C"
+                 ],
+                 correctAnswerIndex: 0)
+    ]
 }
 
