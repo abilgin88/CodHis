df962e4beaf74c584f778e32000157cd281b4d55
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-10 20:10:48 +0300

    Refactoring the Colors into GameColor

---
+++ Code History/Code History/Views/GameColor.swift
@@ -0,0 +1,15 @@
+//
+//  GameColor.swift
+//  CodeHistory
+//
+//  Created by Abdullah Bilgin on 11/10/22.
+
+// Import SwiftUI and define a GameColor structure with four static properties
+import SwiftUI
+
+struct GameColor {
+    static let main = Color(red: 20/255, green: 28/255, blue: 58/255)
+    static let accent = Color(red: 48/255, green: 105/255, blue: 240/255)
+    static let correctGuess = Color.green
+    static let incorrectGuess = Color.red
+}
--- Code History/Code History/Views/WelcomeView.swift
+++ Code History/Code History/Views/WelcomeView.swift
@@ -8,16 +8,11 @@
 import SwiftUI
 
 struct WelcomeView: View {
-    
-    // Redefine the accentColor and mainColor variables
-    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
-    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
-    
     var body: some View {
         NavigationView {
             ZStack {
                 // used inside of WelcomeView and ignore safe areas
-                mainColor.ignoresSafeArea()
+                GameColor.main.ignoresSafeArea()
                 VStack {
                     Spacer()
                     VStack(alignment: .leading, spacing: 0) {
